d85fe3265ea534e71fb9795b47e684bc2d1418cd
Elena <marenina.e@yandex.com>
2022-12-22 12:40:00 +0300

    Задача 05 выполнена

---
+++ Task05/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на ввод
+//принимает одно число (N), а на выходе показывает 
+//все целые числа от -N до N
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+while (count <= number)
+{
+    Console.Write(count + " ");
+    count++;
+}
