568b45fbfd8b7f0a502cb78775a9bf47639c9e20
Elena <marenina.e@yandex.com>
2023-02-16 15:04:44 +0300

    Задача 69 выполнена

---
+++ Task69/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает числа А и В
+// и возводит число А в целую степень В с помощью рекурсии.
+
+Console.WriteLine("Введите натуральное число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("В какую степень будет возводиться число? ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+
+if (numberB > 0)
+{
+    Console.WriteLine(DegreeOfNumber(numberA, numberB));
+}
+else Console.WriteLine("Ошибка ввода степени");
+
+
+int DegreeOfNumber(int numA, int numB)
+{
+    if (numB == 0) return 1;
+    else return DegreeOfNumber(numA, numB - 1) * numA;
+}
